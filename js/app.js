af3bf13329be78e5d3942f04475092344f71a5ff
tgoodall3 <Tylergoodall3@gmail.com>
2023-07-25 18:00:13 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function onsubmitclick() {
+    if (document.getElementById("username").value == "Username" && document.getElementById("password").value == "Password") {
+        document.getElementById("result").innerHTML = "Success";
+    } else {
+        document.getElementById("result").innerHTML = "Wrong Information";
+    }
+
+}
